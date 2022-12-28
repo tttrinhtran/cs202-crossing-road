fbc2f4b70ce5651a09d66e214958318d58e22a60
Gia-Thinh Pham <87513296+pgthinh@users.noreply.github.com>
2022-12-28 20:57:12 +0700

    delete unnecessary code

--- SFML/Game.h
+++
@@ -1,9 +0,0 @@
-#ifndef CROSSING_GAME_H
-#define CROSSING_GAME_H
-
-#include "Header.h"
-
-void UpdateMove( sf::Sprite &rect);
-void setupGame(sf::RenderWindow &window, sf::Event &Event, Player& p);
-
-#endif //CROSSING_GAME_H
